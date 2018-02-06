616eba482f83e588a0dd20952c77b4057d4297c6
chuyalvarez <jesusaagomez@hotmail.com>
2018-02-06 16:01:39 -0600

    Kata2 changes

--- Katas/Kata2/Kata2.c
+++ Katas/Kata2/Kata2.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+int lastrepetition(int i,int pSize,char* s){
+  int c=0;
+  int last=0;
+  int repeated=1;
+  for(int j=i;j<pSize;j++){
+    c = s[j];
+    if (last==c){
+      repeated++;
+    }else{
+      if(repeated>2){
+        return 0;
+      }
+      repeated=1;
+    }
+    last=c;
+  }
+  return 1;
+}
+
 int strongPasswordChecker(char* s) {
 
 int pSize = strlen(s);
@@ -22,7 +41,7 @@ if (pSize < 3){
 
 if(pSize>20){
   changes += pSize-20;
-  //printf("excess change= %d\n",pSize-20);
+  printf("excess change= %d\n",pSize-20);
   buffer = pSize-20;
 }
 
@@ -42,20 +61,26 @@ for (int i = 0;i < pSize;i++){
   if (last==c){
     repeated++;
   }else{
+
     if(repeated>2){
       if(repeated-buffer>2){
-        if(buffer>1){
+        if(lastrepetition(i,pSize,s)){
           repeated-=buffer;
           buffer=0;
+        }else if (repeated%3==0&&repeated>4){
+          repeated-=1;
+          buffer-=1;
         }
         changes+=repeated/3;
-        //printf("repetition change= %d\n",repeated/3);
+        printf("repetition change= %d\n",repeated/3);
         replacebuff+=repeated/3;
 
       }else if(repeated-buffer<3){
         nbuff=repeated-2;
         buffer-=nbuff;
       }
+
+
     }
     repeated=1;
   }
@@ -65,7 +90,7 @@ if(repeated>2){
   if(repeated-buffer>2){
     repeated-=buffer;
     changes+=repeated/3;
-    //printf("repetition change= %d\n",repeated/3);
+    printf("repetition change= %d\n",repeated/3);
     replacebuff+=repeated/3;
     buffer=0;
   }else if(repeated-buffer<3){
@@ -77,20 +102,20 @@ if(repeated>2){
 
 if (upper&&replacebuff<1){
   changes++;
-  //printf("upper change\n");
+  printf("upper change\n");
 }else if(upper){
   replacebuff--;
 }
 if (lower&&replacebuff<1){
   changes++;
-  //printf("lower change\n");
+  printf("lower change\n");
 }else if (lower){
   replacebuff--;
 
 }
 if (digit&&replacebuff<1){
   changes++;
-  //printf("digit change\n");
+  printf("digit change\n");
 }else if (digit){
   replacebuff--;
 }
@@ -101,14 +126,12 @@ if(pSize>2 && pSize<6){
   }
 }
 
-
-
 return changes;
 }
 
 int main() {
 
-  char *password = "aaaabaaaabaaaaa23456A";
+  char *password = "AAAAAABBBBBB123456789a";
   //printf("%s\n",password );
 
   int res = strongPasswordChecker(password);
